70cc820ad9335faa4717da94eb2ca6409addde6f
hamza <hamzakinectro@gmail.com>
2020-02-28 01:19:10 +0500

    routes commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
-import React, { Component } from 'react'
-import {BrowserRouter as Router,Route,Link,} from 'react-router-dom'
-import Modules from './Modules'
-import Home from './Home'
-import { modules } from './data';
-
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Modules from "./Modules";
+import Module from "./Module";
+import Home from "./Home";
+import { modules } from "./data";
 
 export default class App extends Component {
-  state={
-    modules:modules
-  }
-  handleRenderProps=(routerProps) => {
-    return(<Modules {...routerProps} modules={this.state.modules}/>)
-  }
+  state = {
+    modules: modules
+  };
+  handleRenderProps = routerProps => {
+    return <Modules {...routerProps} modules={this.state.modules} />;
+  };
   render() {
     // console.log(this.state.modules)
     return (
       <Router>
-        
-        <div style={{width: 1000, margin: '0 auto'}}>
-        <h1>App Component</h1>
+        <div style={{ width: 1000, margin: "0 auto" }}>
+          <h1>App Component</h1>
           <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/modules'>Modules</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/modules">Modules</Link>
+            </li>
           </ul>
           <hr />
-          <Route exact path='/' component={Home} />
-          <Route path='/modules' render={this.handleRenderProps/*routerProps => <Modules {...routerProps} modules={this.state.modules}/>*/}/>
+          <Route exact path="/" component={Home} />
+
+          <Route
+            exact
+            path="/modules"
+            render={
+              this
+                .handleRenderProps /*routerProps => <Modules {...routerProps} modules={this.state.modules}/>*/
+            }
+          />
+          <Route
+            exact
+            path={`/:moduleId`}
+            render={routerProps => (
+              <Module {...routerProps} modules={modules} />
+            )}
+          />
         </div>
       </Router>
-    )
+    );
   }
 }
-
-
 
 //need render and callback function to pass down to Modules
--- src/Module.js
+++ src/Module.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import Instructor from './Instructor'
-import {BrowserRouter as Router,Route,Link,} from 'react-router-dom'
-export default class Module  extends Component {
+import React, { Component } from "react";
+export default class Module extends Component {
+  render() {
+    const { modules } = this.props;
+    const { match } = this.props;
+    let module = modules.find(
+      element => element.id === parseInt(match.params.moduleId)
+    );
+    console.log("==============", modules, module, match);
 
-    render() { 
-        // console.log(this.props.modules)
-        // console.log(this.props)
-        const modules=this.props.modules
-        let match=this.props.match
-        let module = modules.find(element => element.id === parseInt(match.params.moduleId))  // element.id === match.params.topicId)
+    return (
+      <>
+        {module ? (
+          <>
+            <h1>Module Component </h1>
 
-        return (
-            <Router>
-              <h1>Module Component</h1>
-              <p>{`Module Name: ${module.name}`}</p>
-              <p>{`Main Topic: ${module.description}`}</p>
-        
-              <ul>
-                {module.instructors.map((instructor) => (
-                  <li key={instructor.id}>
-                    <Link to={`${match.url}/${instructor.id}`}>{instructor.name}</Link>
-                  </li>
-                ))}
-              </ul>
-        
-              <hr />
-              <Route exact path={`${match.url}`} render={() => <h3>{'Choose a instructor from the list above (by default)'}</h3>}/>
-              <Route path={`${match.path}/:instructorId`} render={routerProps => <Instructor {...routerProps} modules={modules} /> }/>
-            </Router>
-          );
-    }
+            <p>{`Module Name: ${module.name}`}</p>
+            <p>{`Main Topic: ${module.description}`}</p>
+            <button
+              onClick={() => {
+                this.props.history.push(`/modules`);
+              }}
+            >
+              Back
+            </button>
+          </>
+        ) : (
+          ""
+        )}
+      </>
+    );
+  }
 }
